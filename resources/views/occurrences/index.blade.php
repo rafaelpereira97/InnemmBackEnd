221bde1fbdefa421a7fa974a716912e7728498b9
Rafael <rafaelpereira_97@outlook.com>
2021-04-26 20:43:20 +0100

    dashboard, index of occurrences

---
+++ resources/views/occurrences/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.backend')
+
+@section('content')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>INNEMM - Dashboard</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Blank Page</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                Ocorrencias
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('dashboard')->group(function(){
+    Route::prefix('occurrences')->group(function(){
+        Route::get('index')->name('occurrence.index');
+    });
+});
