b9d48e65355faea696b954423173d5fd7509bc2e
Rafael <rafaelpereira_97@outlook.com>
2021-05-26 00:53:13 +0100

    Fix do admin lte

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -12,6 +12,11 @@
     <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+    <style>
+        .content-wrapper{
+            height: initial;
+        }
+    </style>
 </head>
 <body class="hold-transition sidebar-mini">
 
