4b2b397dd4e0076c4dea9d101c8bfcec4bb5e2bd
rafaelpereira <rafaelpereira_97@hotmail.com>
2021-06-25 16:14:35 +0100

    CRUD

--- app/Http/Controllers/API/OccurrenceController.php
+++ app/Http/Controllers/API/OccurrenceController.php
@@ -162,7 +162,7 @@ class OccurrenceController extends Controller
     public function occurrencesAcceptedByMonth(Request $request){
         $user = User::find($request->user()->id);
 
-        $months = [01,02,03,04,05,06,07,8,9,10,11,12];
+        $months = ["01","02","03","04","05","06","07","08","09","10","11","12"];
 
         $array = array();
 
